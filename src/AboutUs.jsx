466a67954c945599f26307ea48f3dccdda8822d5
edu1brito <edudbc483@gmail.com>
2025-04-22 12:27:05 -0300

    primeiro commit

---
+++ src/AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {/* <h1 className="about-us-heading">About Us</h1> */}
+      <p className="about-us-description">Welcome to Zen Garden, where Nature Finds a Home!</p>
+      <p className="about-us-content">
+        At Zen Garden, we believe in the power of nature to transform spaces and lives. Our mission is to connect people with the beauty, calm, and benefits of plants through a curated selection of high-quality, sustainable greenery.
+      </p>
+      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      <p className="about-us-content">
+        Our team at Zen Garden is committed to ensuring that every plant meets our high standards for quality and care. Whether you're an experienced gardener or just beginning your plant journey, we’re here to guide and support you. Explore our curated collection, reach out with any questions, and let us help you bring life and beauty to your space.
+      </p>
+      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+
+      <p className="about-us-content">
+        Join us in our mission to create a greener, healthier world. Visit VerdeVibe today and experience the joy of nature right at your doorstep.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
